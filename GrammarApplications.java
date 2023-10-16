0fcb7ec93621be84b8a84e90ff51a99967bc76b5
Kyle Buhay <134907711+kylebuhay@users.noreply.github.com>
2023-10-16 21:18:46 +0800

    Update GrammarApplications.java

--- GrammarApplications.java
+++ GrammarApplications.java
@@ -3,11 +3,12 @@ package grammarapplications;
 import java.util.Scanner;
 
 public class GrammarApplications {
-
     public static void main(String[] args) {
        Scanner scan = new Scanner(System.in);
-       
-        System.out.print("enter user input: ");
+        System.out.println("------------------------------------------------------");
+        System.out.println("| Welcome to the Assignment Statement Syntax Checker |");
+        System.out.println("------------------------------------------------------");
+        System.out.print("Enter user input: ");
         
         String assignment = scan.nextLine();
         System.out.println("String: " + assignment);
