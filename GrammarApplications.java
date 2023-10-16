9aea548a662b20859c54b8ee253083e95c7cd8e7
Kyle Buhay <134907711+kylebuhay@users.noreply.github.com>
2023-10-16 22:12:39 +0800

    Update GrammarApplications.java

--- GrammarApplications.java
+++ GrammarApplications.java
@@ -4,14 +4,24 @@ import java.util.Scanner;
 
 public class GrammarApplications {
     public static void main(String[] args) {
-       Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.println("------------------------------------------------------");
         System.out.println("| Welcome to the Assignment Statement Syntax Checker |");
         System.out.println("------------------------------------------------------");
         System.out.print("Assignment Statement to check: ");
-        
+
         String assignment = scan.nextLine();
         System.out.println();
         System.out.println("Assignment Statement: " + assignment);
+        String result = semiColonChecker(assignment);
+        System.out.println(result);
+    }
+
+    public static String semiColonChecker(String assignment) {
+        if (assignment.endsWith(";")) {
+            return "Valid Statement.";
+        } else {
+            return "Error: Missing a semicolon.";
+        }
     }
 }
